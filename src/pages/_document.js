236a8976321ed1e7ba5a74a3c748a038b02c28b3
ghazwan888 <ghazwanrkn@gmail.com>
2026-03-19 20:21:06 +0300

    Create _document.js

---
+++ src/pages/_document.js
@@ -0,0 +1,11 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta name="google-site-verification" content="mJcfIBBqvrRBixq67fXOfVMI48r1fPVZrtoJUv68nCw" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
